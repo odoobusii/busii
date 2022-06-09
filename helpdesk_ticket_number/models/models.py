e457046aa98ac9e3bcff39a50f61ea66681c53c9
wernerbusii <werner@busii.com>
2022-06-09 16:05:55 +0200

    init

---
+++ helpdesk_ticket_number/models/models.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+from odoo import api, fields, models
+import re
+
+
+class HelpDeskTicketNumber(models.Model):
+    
+    _inherit = 'helpdesk.ticket'
+    
+    ticket_number = fields.Integer(compute='_compute_ticket_number',store=True)
+    
+    @api.depends('name')
+    def _compute_ticket_number(self):
+        regex=re.compile("\(\#([0-9]*)\)")
+        for ticket in self:
+            ticket.ticket_number=int(regex.search(ticket.display_name).group(1))
+            
