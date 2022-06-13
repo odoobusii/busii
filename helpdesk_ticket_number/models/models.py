a7a92a70edc10108cd839009a6e1d060d3ab129d
wernerbusii <werner@busii.com>
2022-06-13 15:08:23 +0200

    ticket no changed to ticket_id

--- helpdesk_ticket_number/models/models.py
+++ helpdesk_ticket_number/models/models.py
@@ -3,19 +3,19 @@
 # from odoo import models, fields, api
 
 
-from odoo import api, fields, models
-import re
+# from odoo import api, fields, models
+# import re
 
 
-class HelpDeskTicketNumber(models.Model):
+# class HelpDeskTicketNumber(models.Model):
     
-    _inherit = 'helpdesk.ticket'
+#     _inherit = 'helpdesk.ticket'
     
-    ticket_number = fields.Integer(compute='_compute_ticket_number',store=True)
+#     ticket_number = fields.Integer(compute='_compute_ticket_number',store=True)
     
-    @api.depends('name')
-    def _compute_ticket_number(self):
-        regex=re.compile("\(\#([0-9]*)\)")
-        for ticket in self:
-            ticket.ticket_number=int(regex.search(ticket.display_name).group(1))
+#     @api.depends('name')
+#     def _compute_ticket_number(self):
+#         regex=re.compile("\(\#([0-9]*)\)")
+#         for ticket in self:
+#             ticket.ticket_number=int(regex.search(ticket.display_name).group(1))
             
