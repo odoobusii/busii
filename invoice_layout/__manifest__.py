21ca687695f6c3c6ce44b9085022df4ac27c01ec
Werner Crous <werner@busii.com>
2023-04-05 08:18:48 +0200

    changed for v16

--- invoice_layout/__manifest__.py
+++ invoice_layout/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Customizations',
-    'version': '15.0.0.0.1',
+    'version': '16.0.0.0.1',
     'license': 'LGPL-3',
     'installable': True,
     'application': False,
